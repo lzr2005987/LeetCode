6cb602ba6e616f5a67c3fbb7bad57dde352f7efb
lizheren <lizheren@zofund.com>
2019-09-09 14:35:54 +0800

    69/71

---
+++ app/src/main/java/com/leetcode/No71.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No71 {
+    public String simplifyPath(String path) {
+        String[] arr = path.split("/");
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            String s = arr[i];
+            if (i != 0 && s.length() == 0 || s.equals(".")) {
+                continue;
+            }
+            if (s.equals("..")) {
+                int a = list.size();
+                if (a > 0 && !list.get(a - 1).equals(""))
+                    list.remove(a - 1);
+            } else {
+                list.add(s);
+            }
+        }
+        if (list.isEmpty())
+            return "/";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : list) {
+            stringBuilder.append(s).append("/");
+        }
+        int a = stringBuilder.length();
+        return  a > 1 ? stringBuilder.substring(0, a - 1) : stringBuilder.toString();
+    }
+}
