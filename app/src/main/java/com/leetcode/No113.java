67165bfd78cc566e7bc73f3aceb9cfcbc30ef44c
lizheren <lizheren@zofund.com>
2019-11-22 13:34:58 +0800

    80/112/113 tree

---
+++ app/src/main/java/com/leetcode/No113.java
@@ -0,0 +1,44 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No113 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private int sum;
+    private List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return result;
+        }
+        this.sum = sum;
+        List<Integer> list = new ArrayList<Integer>();
+        search(0, root, list);
+        return result;
+    }
+
+    private void search(int cur, TreeNode node, List<Integer> list) {
+        cur += node.val;
+        list.add(node.val);
+        if (cur == sum && node.left == null && node.right == null) {
+            result.add(list);
+        } else {
+            if (node.left != null) {
+                search(cur, node.left, new ArrayList<Integer>(list));
+            }
+            if (node.right != null) {
+                search(cur, node.right, new ArrayList<Integer>(list));
+            }
+        }
+    }
+}
