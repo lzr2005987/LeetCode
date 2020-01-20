db5407b533d4c293764ac5a2a25682a89d3a9cb2
lizheren <lizheren@zofund.com>
2020-01-20 15:53:13 +0800

    N221，第100题AC

---
+++ app/src/main/java/com/leetcode/No129.java
@@ -0,0 +1,37 @@
+package com.leetcode;
+
+public class No129 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    int sum = 0;
+
+    public int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        getNum("", root);
+        return sum;
+    }
+
+    private void getNum(String num, TreeNode node) {
+        if (node.left == null && node.right == null) {
+            sum += Integer.parseInt(num + node.val);
+        } else {
+            num = num + node.val;
+            if (node.left != null) {
+                getNum(num, node.left);
+            }
+            if (node.right != null) {
+                getNum(num, node.right);
+            }
+        }
+    }
+}
