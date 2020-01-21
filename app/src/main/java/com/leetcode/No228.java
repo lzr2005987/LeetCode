41787e4e97181e2d45864fe886c05d88f8f0f00b
lizheren <lizheren@zofund.com>
2020-01-21 17:59:00 +0800

    No205/206/226/228/229/257/260/263

---
+++ app/src/main/java/com/leetcode/No228.java
@@ -0,0 +1,37 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No228 {
+    public List<String> summaryRanges(int[] nums) {
+        if (nums.length == 0) {
+            return new ArrayList<String>();
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(nums[0] + "");
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i < nums.length; i++) {
+            if (sb.length() == 0) {
+                sb.append(nums[i] + "");
+            }
+            if (nums[i] - 1 == nums[i - 1] && !sb.toString().contains("->")) {
+                sb.append("->");
+            }
+            if (nums[i] - 1 > nums[i - 1]) {
+                if (sb.toString().contains("->")) {
+                    sb.append(nums[i - 1] + "");
+                }
+                list.add(sb.toString());
+                sb = new StringBuilder(nums[i] + "");
+            }
+        }
+        if (sb.toString().contains("->")) {
+            sb.append(nums[nums.length - 1] + "");
+            list.add(sb.toString());
+        } else if (sb.length() > 0) {
+            list.add(sb.toString());
+        }
+        return list;
+    }
+}
---
+++ app/src/main/java/com/leetcode/No257.java
@@ -0,0 +1,43 @@
+package com.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No257 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    List<String> res = new ArrayList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<String>();
+        }
+        search(root, "");
+        return res;
+    }
+
+    private void search(TreeNode node, String sb) {
+        sb = sb + node.val;
+        if (node.left != null) {
+            sb = sb + "->";
+            search(node.left, sb);
+        }
+        if (node.right != null) {
+            if (node.left == null) {
+                sb = sb + "->";
+            }
+            search(node.right, sb);
+        }
+        if (node.left == null && node.right == null) {
+            res.add(sb);
+        }
+    }
+}
