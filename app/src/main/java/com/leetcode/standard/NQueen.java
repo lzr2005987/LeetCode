dd815fee37669fc6e13008da250c314adaee16a5
lizheren <lizheren@zofund.com>
2020-08-21 18:10:19 +0800

    补充注释

--- app/src/main/java/com/leetcode/standard/NQueen.java
+++ app/src/main/java/com/leetcode/standard/NQueen.java
@@ -9,26 +9,51 @@ public class NQueen {
         return ans;
     }
 
+    /**
+     * 回溯放置棋子，计算方案总数
+     *
+     * @param position
+     * @param n
+     * @param cur
+     */
     private void backtrack(int[][] position, int n, int cur) {
         for (int i = 0; i < n; i++) {
-            if (check(position, cur, i, cur)) {
-                if (cur == n - 1) {
+            if (check(position, cur, i, cur)) {//检查当前位置是否能放
+                if (cur == n - 1) {//方案数量+1
                     ans++;
                     return;
                 }
+                //放置棋子
                 position[cur][0] = cur;
                 position[cur][1] = i;
+                //回溯
                 backtrack(copy(position, cur), n, cur + 1);
             }
         }
     }
 
+    /**
+     * 深拷贝用于回溯的数组
+     *
+     * @param origin
+     * @param cur
+     * @return
+     */
     private int[][] copy(int[][] origin, int cur) {
         int[][] newArray = new int[origin.length][2];
         System.arraycopy(origin, 0, newArray, 0, cur + 1);
         return newArray;
     }
 
+    /**
+     * 检查当前位置是否可以放置
+     *
+     * @param position
+     * @param i
+     * @param j
+     * @param cur
+     * @return
+     */
     private boolean check(int[][] position, int i, int j, int cur) {
         for (int k = 0; k < cur; k++) {
             int x = position[k][0];
